f710f5f465bd94693ae77c2ea3525883d40a8c73
Akshay Vivekananda B <akshay9480606723@gmail.com>
2024-08-03 18:20:37 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Nav from './components/nav'
+
+function Home() {
+
+  return (
+    <div className='h-[100vh]'>
+      <Nav />
+      <h1>Hello world</h1>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+      <div>
+        Hello AK-GK
+      </div>
+    </>
+  )
+}
+
+export default Nav
